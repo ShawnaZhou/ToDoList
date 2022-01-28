e8ae775282fa0ed0127a94c244e353e36c77012f
ShawnaZhou <2359871499@qq.com>
2022-01-28 17:31:44 +0800

    feat: few influence

--- src/components/AddItem.vue
+++ src/components/AddItem.vue
@@ -1,90 +1,85 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { ElMessage } from "element-plus";
-import bus from "vue3-eventbus";
-import { CirclePlusFilled } from "@element-plus/icons-vue";
+import { ref } from 'vue';
+import { ElMessage } from 'element-plus';
+import bus from 'vue3-eventbus';
+import { CirclePlusFilled } from '@element-plus/icons-vue';
 
-const input = ref("");
-const time = ref("");
+const input = ref('');
+const time = ref('');
 
 const messageShow = (content: string, msgType: any) => {
-  ElMessage({
-    message: content,
-    type: msgType,
-  });
+	ElMessage({
+		message: content,
+		type: msgType,
+	});
 };
 
 const defaultTime = new Date();
 const send = () => {
-  if (input.value.length === 0) {
-    messageShow("oops you haven't input any things you want to do", "error");
-    return;
-  }
-  if (time.value.length === 0) {
-    messageShow("oops you haven't choose any time yet", "error");
-    return;
-  }
-  messageShow("Add success", "success");
-  let tempData = {
-    time: time.value,
-    data: input.value,
-  };
-  console.log(tempData);
-  bus.emit("addItem", tempData);
-  input.value = "";
-  time.value = "";
+	if (input.value.length === 0) {
+		messageShow("oops you haven't input any things you want to do", 'error');
+		return;
+	}
+	if (time.value.length === 0) {
+		messageShow("oops you haven't choose any time yet", 'error');
+		return;
+	}
+	messageShow('Add success', 'success');
+	let tempData = {
+		time: time.value,
+		data: input.value,
+	};
+	console.log(tempData);
+	bus.emit('addItem', tempData);
+	input.value = '';
+	time.value = '';
 };
 </script>
 
 <template>
-  <div class="container">
-    <h1>Add Item</h1>
-    <el-icon color="lightgreen" :size="30">
-      <CirclePlusFilled />
-    </el-icon>
-    <el-row class="firstRow">
-      <h3>Choose time</h3>
-      <el-date-picker
-        v-model="time"
-        type="datetime"
-        placeholder="Select date and time"
-        :default-time="defaultTime"
-      >
-      </el-date-picker>
-    </el-row>
-    <el-row class="firstRow">
-      <h3>What need to do</h3>
-      <el-input v-model="input" placeholder="Type items you need to do" />
-    </el-row>
-    <el-row class="firstRow">
-      <button @click="send" class="btnConfirm">Confirm</button>
-    </el-row>
-  </div>
+	<div class="container">
+		<h1>Add Item</h1>
+		<el-icon color="lightgreen" :size="30">
+			<CirclePlusFilled />
+		</el-icon>
+		<el-row class="firstRow">
+			<h3>Choose time</h3>
+			<el-date-picker v-model="time" value-format="YYYY/MM/DD hh:mm:ss" type="datetime" placeholder="Select date and time" :default-time="defaultTime">
+			</el-date-picker>
+		</el-row>
+		<el-row class="firstRow">
+			<h3>What need to do</h3>
+			<el-input v-model="input" placeholder="Type items you need to do" />
+		</el-row>
+		<el-row class="firstRow">
+			<button @click="send" class="btnConfirm">Confirm</button>
+		</el-row>
+	</div>
 </template>
 
 <style scoped>
 .container {
-  margin: 1rem 0;
-  width: 96%;
-  height: 100%;
-  padding: 1rem 2%;
-  background-color: #353535;
-  color: #fff;
+	margin: 1rem 0;
+	width: 96%;
+	height: 100%;
+	padding: 1rem 2%;
+	background-color: #353535;
+	color: #fff;
 }
 .firstRow {
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  margin-bottom: 1rem;
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	margin-bottom: 1rem;
 }
 .btnConfirm {
-  margin-top: 1rem;
-  margin-bottom: 0;
-  background: #000;
-  width: 8rem;
+	margin-top: 1rem;
+	margin-bottom: 0;
+	background: #000;
+	width: 8rem;
 }
 .btnConfirm:hover {
-  background: #f5f5f5;
-  color: #000;
+	background: #f5f5f5;
+	color: #000;
 }
 </style>
--- src/components/DoneList.vue
+++ src/components/DoneList.vue
@@ -1,95 +1,82 @@
 <template>
-  <div class="container">
-    <h1>Done List</h1>
-    <el-icon color="lightgreen" :size="30">
-      <CircleCheckFilled />
-    </el-icon>
-    <el-table
-      ref="multipleTableRef"
-      class="tableContainer"
-      max-height="400"
-      row-key="time"
-      key="time"
-      stripe
-      :data="tableData"
-      :default-sort="{ prop: 'time', order: 'ascending' }"
-      style="width: 100%"
-      @selection-change="handleSelectionChange"
-    >
-      <el-table-column type="selection" width="55" />
-      <el-table-column
-        prop="time"
-        label="Time"
-        sortable
-        width="180"
-        column-key="time"
-      />
-      <el-table-column property="data" label="Things have done" />
-    </el-table>
-    <div class="tableFooter">
-      <button
-        class="btnDelete"
-        @click="deleteThings"
-        v-show="multipleSelection.length > 0"
-      >
-        Delete
-      </button>
-    </div>
-  </div>
+	<div class="container">
+		<h1>Done List</h1>
+		<el-icon color="lightgreen" :size="30">
+			<CircleCheckFilled />
+		</el-icon>
+		<el-table
+			ref="multipleTableRef"
+			class="tableContainer"
+			max-height="400"
+			row-key="time"
+			key="time"
+			stripe
+			:data="tableData"
+			:default-sort="{ prop: 'time', order: 'ascending' }"
+			style="width: 100%"
+			@selection-change="handleSelectionChange"
+		>
+			<el-table-column type="selection" width="55" />
+			<el-table-column prop="time" label="Time" sortable width="180" column-key="time" />
+			<el-table-column property="data" label="Things have done" />
+		</el-table>
+		<div class="tableFooter">
+			<button class="btnDelete" @click="deleteThings" v-show="multipleSelection.length > 0">Delete</button>
+		</div>
+	</div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue";
-import bus from "vue3-eventbus";
-import type { ElTable } from "element-plus";
-import { CircleCheckFilled } from "@element-plus/icons-vue";
+import { ref, onMounted } from 'vue';
+import bus from 'vue3-eventbus';
+import type { ElTable } from 'element-plus';
+import { CircleCheckFilled } from '@element-plus/icons-vue';
 
 interface ToDo {
-  time: string;
-  data: string;
+	time: string;
+	data: string;
 }
 
 const multipleTableRef = ref<InstanceType<typeof ElTable>>();
 const multipleSelection = ref<ToDo[]>([]);
 const tableData = ref<ToDo[]>([]);
 
-const init = () => {
-  let tempData = localStorage.getItem("doneList");
-  if (tempData) {
-    tableData.value = tableData.value.concat(JSON.parse(tempData));
-  }
-};
-init();
+onMounted(() => {
+	let tempData = localStorage.getItem('toDoList');
+	if (tempData) {
+		tableData.value = tableData.value.concat(JSON.parse(tempData));
+	}
+});
 
-bus.on("doneThings", (e: ToDo[]) => {
-  tableData.value = tableData.value.concat(e);
-  localStorage.setItem("doneList", JSON.stringify(tableData.value));
+bus.on('doneThings', (e: ToDo[]) => {
+	tableData.value = tableData.value.concat(e);
+	localStorage.setItem('doneList', JSON.stringify(tableData.value));
 });
 
 const handleSelectionChange = (val: ToDo[]) => {
-  multipleSelection.value = val;
+	multipleSelection.value = val;
 };
 
 const deleteRow = (index: number) => {
-  tableData.value.splice(index, 1);
-  localStorage.setItem("doneList", JSON.stringify(tableData.value));
+	tableData.value.splice(index, 1);
+	localStorage.setItem('doneList', JSON.stringify(tableData.value));
 };
 
 const deleteThings = () => {
-  multipleSelection.value.forEach((item, index) => {
-    let tempIndex = tableData.value.indexOf(item);
-    deleteRow(tempIndex);
-  });
+	multipleSelection.value.forEach((item, index) => {
+		let tempIndex = tableData.value.indexOf(item);
+		deleteRow(tempIndex);
+	});
 };
 </script>
 
 <style>
 .container {
-  margin: 1rem 0;
-  width: 96%;
-  height: 100%;
-  padding: 1rem 2%;
-  background-color: #353535;
-  color: #fff;
+	margin: 1rem 0;
+	width: 96%;
+	height: 100%;
+	padding: 1rem 2%;
+	background-color: #353535;
+	color: #fff;
 }
 </style>
